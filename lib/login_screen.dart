890d6cd2e401c3352c6136a4095d9a8cf38b0b31
sashhhita <sashhhita@gmail.com>
2020-10-02 23:22:52 -0500

    New icons added. Also fonts and other interface stuff changed, nothing to worry 'bout, k?

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -4,30 +4,12 @@ import 'register_screen.dart';
 // Import the firebase_core plugin
 import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(Buku());
-
-class Buku extends StatelessWidget{
+class LoginScreen extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: <String, WidgetBuilder>{
-        '/register':(BuildContext context)  => registerScreen()
-      },
-      home: initScreen(),
-
-    );
-  }
-
+  _LoginScreenState createState() => _LoginScreenState();
 }
 
-class initScreen extends StatefulWidget{
-  @override
-  _initScreenState createState() => _initScreenState();
-}
-
-class _initScreenState extends State<initScreen>{
+class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,92 +19,102 @@ class _initScreenState extends State<initScreen>{
         padding: EdgeInsets.only(top: 50.0),
         child: Column(
           children: <Widget>[
-
             Image(
-              image: AssetImage('assets/images/firebase.png'),
-              width: 200.0,
-              height: 200.0,
+              image: AssetImage('assets/images/bukunobackgroundfull.PNG'),
+              width: 250.0,
+              height: 250.0,
             ),
-
             Container(
-              padding: EdgeInsets.only(left: 30.0,right: 30.0,top: 20.0),
+              padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 20.0),
               child: Column(
-
                 children: <Widget>[
-
                   TextField(
                     decoration: InputDecoration(
-                      labelText: 'EMAIL',
-                      labelStyle: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.orange)
-                      )
-                    ),
+                        labelText: 'EMAIL',
+                        labelStyle: TextStyle(
+                            fontFamily: 'ProductSans',
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.orange))),
                   ),
-                  SizedBox(height: 20.0,),
+                  SizedBox(
+                    height: 20.0,
+                  ),
                   TextField(
                     decoration: InputDecoration(
                       labelText: 'PASSWORD',
-                      labelStyle: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
+                      labelStyle: TextStyle(
+                          color: Colors.grey, fontWeight: FontWeight.bold),
                       focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.orange)
-                      ),
+                          borderSide: BorderSide(color: Colors.orange)),
                     ),
                     obscureText: true,
                   )
                 ],
               ),
             ),
-
-            SizedBox(height: 50.0,),
+            SizedBox(
+              height: 50.0,
+            ),
             Container(
-              padding: EdgeInsets.only(left: 30.0,right: 30.0),
+              padding: EdgeInsets.only(left: 30.0, right: 30.0),
               height: 50.0,
               child: Material(
-
                 borderRadius: BorderRadius.circular(20.0),
                 shadowColor: Colors.orangeAccent,
                 color: Colors.orange,
                 elevation: 7.0,
                 child: GestureDetector(
-
-                  onTap: (){},
+                  onTap: () {},
                   child: Center(
                     child: Text(
                       'LOGIN',
-                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                      style: TextStyle(
+                          fontSize: 18.0,
+                          fontFamily: 'ProductSans',
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
                     ),
                   ),
                 ),
               ),
             ),
-            SizedBox(height: 50.0,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-
-                Text(
-                  'New in Buku?',
-                  style: TextStyle(fontWeight: FontWeight.bold)
-                ),
-                SizedBox(width: 10.0,),
-                InkWell(
-                  onTap: (){
-                    Navigator.of(context).pushNamed('/register');
-                  },
-                  child: Text(
-                    'Register Now',
-                    style: TextStyle(color: Colors.orange, fontWeight: FontWeight.bold, decoration: TextDecoration.underline),
+            SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 30.0, right: 30.0),
+              height: 50.0,
+              color: Colors.transparent,
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.of(context).pushNamed('/register');
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: Colors.black,
+                        style: BorderStyle.solid,
+                        width: 2.0),
+                    color: Colors.transparent,
+                    borderRadius: BorderRadius.circular(20.0),
                   ),
-                )
-              ],
-            )
+                  child: Center(
+                    child: Text(
+                      'Register',
+                      style: TextStyle(
+                          fontSize: 16.0,
+                          fontFamily: 'ProductSans',
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
-
         ),
       ),
-
     );
   }
 }
-
--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -3,134 +3,144 @@ import 'package:flutter/material.dart';
 // Import the firebase_core plugin
 import 'package:firebase_core/firebase_core.dart';
 
-class registerScreen extends StatefulWidget{
+class registerScreen extends StatefulWidget {
   @override
   _registerScreenState createState() => _registerScreenState();
-  }
+}
 
-class _registerScreenState extends State<registerScreen>{
+class _registerScreenState extends State<registerScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomPadding: false,
-      backgroundColor: Colors.orange,
-      body: Container(
-        
-        padding: EdgeInsets.only(top: 40.0),
-        child: Column(
-          children: [
-
-            Image(
-              image: AssetImage('assets/images/firebase.png'),
-              width: 150.0,
-              height: 150.0,
-            ),
-            SizedBox(height: 20,),
-            Expanded(
-              child: Container(
-                  //color: Colors.white,
-                padding: EdgeInsets.only(top: 30, left: 30, right: 30),
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(40.0),
-                      topRight: Radius.circular(40.0),
-                    )
-                  ),
-                  child: Column(
-                    children: <Widget>[
-
-                      TextField(
-                        decoration: InputDecoration(
-                            labelText: 'EMAIL',
-                            labelStyle: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
-                            focusedBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(color: Colors.orange)
-                            )
-                        ),
-                      ),
-                      SizedBox(height: 20.0,),
-                      TextField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                            labelText: 'PASSWORD',
-                            labelStyle: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
-                            focusedBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(color: Colors.orange)
-                            )
-                        ),
-                      ),
-                      SizedBox(height: 20.0,),
-                      TextField(
-                        decoration: InputDecoration(
-                            labelText: 'NICK NAME',
-                            labelStyle: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
-                            focusedBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(color: Colors.orange)
-                            )
-                        ),
-                      ),
-
-                      SizedBox(height: 50.0,),
-
-                      Container(
-                        //padding: EdgeInsets.only(left: 30.0,right: 30.0),
-                        height: 50.0,
-                        child: Material(
-
-                          borderRadius: BorderRadius.circular(20.0),
-                          shadowColor: Colors.orangeAccent,
-                          color: Colors.orange,
-                          elevation: 7.0,
-                          child: GestureDetector(
-
-                            onTap: (){},
-                            child: Center(
-                              child: Text(
-                                'REGISTER',
-                                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 20.0,),
-                      Container(
-                        //padding: EdgeInsets.only(left: 30.0,right: 30.0),
-                        height: 50.0,
-                        color: Colors.transparent,
-                        child: GestureDetector(
-                          onTap: (){
-                            Navigator.of(context).pop();
-                          },
-                          child: Container(
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                  color: Colors.black,
-                                  style: BorderStyle.solid,
-                                  width: 2.0
-                              ),
-                              color: Colors.transparent,
-                              borderRadius: BorderRadius.circular(20.0),
-                            ),
-                            child: Center(
-                              child: Text(
-                                'Go Back',
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-
-                    ],
-                  )
+        resizeToAvoidBottomPadding: false,
+        backgroundColor: Colors.orange,
+        body: Container(
+          padding: EdgeInsets.only(top: 40.0),
+          child: Column(
+            children: [
+              Image(
+                image: AssetImage('assets/images/bukusymbol.png'),
+                width: 200.0,
+                height: 200.0,
               ),
-            )
-          ],
-        ),
-      )
-    );
+              SizedBox(
+                height: 20,
+              ),
+              Expanded(
+                child: Container(
+                    //color: Colors.white,
+                    padding: EdgeInsets.only(top: 30, left: 30, right: 30),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(40.0),
+                          topRight: Radius.circular(40.0),
+                        )),
+                    child: Column(
+                      children: <Widget>[
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: 'EMAIL',
+                              labelStyle: TextStyle(
+                                  fontFamily: 'ProductSans',
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.bold),
+                              focusedBorder: UnderlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Colors.orange))),
+                        ),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        TextField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                              labelText: 'PASSWORD',
+                              labelStyle: TextStyle(
+                                  fontFamily: 'ProductSans',
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.bold),
+                              focusedBorder: UnderlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Colors.orange))),
+                        ),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: 'NICK NAME',
+                              labelStyle: TextStyle(
+                                  fontFamily: 'ProductSans',
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.bold),
+                              focusedBorder: UnderlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Colors.orange))),
+                        ),
+                        SizedBox(
+                          height: 50.0,
+                        ),
+                        Container(
+                          //padding: EdgeInsets.only(left: 30.0,right: 30.0),
+                          height: 50.0,
+                          child: Material(
+                            borderRadius: BorderRadius.circular(20.0),
+                            shadowColor: Colors.orangeAccent,
+                            color: Colors.orange,
+                            elevation: 7.0,
+                            child: GestureDetector(
+                              onTap: () {},
+                              child: Center(
+                                child: Text(
+                                  'REGISTER',
+                                  style: TextStyle(
+                                      fontSize: 18.0,
+                                      fontFamily: 'ProductSans',
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        Container(
+                          //padding: EdgeInsets.only(left: 30.0,right: 30.0),
+                          height: 50.0,
+                          color: Colors.transparent,
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                    color: Colors.black,
+                                    style: BorderStyle.solid,
+                                    width: 2.0),
+                                color: Colors.transparent,
+                                borderRadius: BorderRadius.circular(20.0),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'Go Back',
+                                  style: TextStyle(
+                                      fontFamily: 'ProductSans',
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )),
+              )
+            ],
+          ),
+        ));
   }
 }
